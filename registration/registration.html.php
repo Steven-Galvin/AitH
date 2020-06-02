8cd6ed40bdc6954c71fac83304efd2ab14708b56
Steven Galvin <steven.john.galvin@gmail.com>
2020-06-01 18:41:40 -0700

    [Feature] Add Google Analytics to all pages

--- registration/registration.html.php
+++ registration/registration.html.php
@@ -12,6 +12,17 @@
 
 	<head>
 
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168243744-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-168243744-1');
+		</script>
+
+
 		<meta charset="UTF-8">
 
 		<title>Ace in the Hole: Registration</title>
